f1c5a6d2974648a543a89b437a9001f29baa7bd5
Rachyl <rachylquan@yahoo.com>
2021-04-15 23:45:44 -0700

    expiry

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -1,21 +1,64 @@
-import config from '../config'
+import jwtDecode from 'jwt-decode';
+import config from '../config';
+
+let _timeoutId;
+const _TEN_SECONDS_IN_MS = 1000;
 
 const TokenService = {
   saveAuthToken(token) {
-    window.localStorage.setItem(config.TOKEN_KEY, token)
+    window.sessionStorage.setItem(config.TOKEN_KEY, token);
   },
-  getAuthToken() {
-    return window.localStorage.getItem(config.TOKEN_KEY)
-  },
-  clearAuthToken() {
-    window.localStorage.removeItem(config.TOKEN_KEY)
-  },
-  hasAuthToken() {
-    return !!TokenService.getAuthToken()
-  },
-  makeBasicAuthToken(userName, password) {
-    return window.btoa(`${userName}:${password}`)
-  },
-}
 
-export default TokenService
+  getAuthToken() {
+    return window.sessionStorage.getItem(config.TOKEN_KEY);
+  },
+
+  clearAuthToken() {
+    console.log('clearing the auth token');
+    window.sessionStorage.removeItem(config.TOKEN_KEY);
+  },
+
+  hasAuthToken() {
+    return !!TokenService.getAuthToken();
+  },
+
+  makeBasicAuthToken(userName, password) {
+    return window.btoa(`${userName}:${password}`);
+  },
+
+  parseJwt(jwt) {
+    return jwtDecode(jwt);
+  },
+
+  readJwtToken() {
+    return TokenService.parseJwt(TokenService.getAuthToken());
+  },
+
+  _getMsUntilExpiry(payload) {
+    /*
+      payload is from the JWT
+      the 'exp' value is in seconds, need to conver to ms, so * 1000
+      calculates the difference between now and when the JWT will expire
+    */
+    return payload.exp * 1000 - Date.now();
+  },
+
+  queueCallbackBeforeExpiry(callback) {
+    /* get the number of ms from now until the token expires */
+    const msUntilExpiry = TokenService._getMsUntilExpiry(
+      TokenService.readJwtToken()
+    );
+    /*
+      queue a callback that will happen 10 seconds before the token expires
+      the callback is passed in as an argumanet so could be anything,
+      in this app, the callback is for calling the refresh endpoint
+    */
+    _timeoutId = setTimeout(callback, msUntilExpiry - _TEN_SECONDS_IN_MS);
+  },
+
+  clearCallbackBeforeExpiry() {
+    clearTimeout(_timeoutId);
+  },
+};
+
+export default TokenService;
